f7ccb15e795f4bec9ae55d6b806d8d8a4b8a3830
Baytars <feicuihuangfeng@qq.com>
2019-08-17 23:16:28 +0800

    Implemented embeded IE browser

--- src/cn/gedobu/some/embeded/browser/parts/SoMeBrowserView.java
+++ src/cn/gedobu/some/embeded/browser/parts/SoMeBrowserView.java
@@ -9,19 +9,122 @@ import org.eclipse.e4.ui.services.IServiceConstants;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.SWTError;
+import org.eclipse.swt.browser.Browser;
+import org.eclipse.swt.browser.LocationListener;
+import org.eclipse.swt.browser.ProgressEvent;
+import org.eclipse.swt.browser.ProgressListener;
+//import org.eclipse.swt.chromium.Browser;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.ProgressBar;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.widgets.ToolBar;
+import org.eclipse.swt.widgets.ToolItem;
 
 public class SoMeBrowserView {
 	private Label myLabelInView;
 
 	@PostConstruct
 	public void createPartControl(Composite parent) {
-		System.out.println("Enter in SampleE4View postConstruct");
+		GridLayout gridLayout = new GridLayout();
+		gridLayout.numColumns = 3;
+		parent.setLayout(gridLayout);
+		ToolBar toolbar = new ToolBar(parent, SWT.NONE);
+		ToolItem itemBack = new ToolItem(toolbar, SWT.PUSH);
+		itemBack.setText("Back");
+		ToolItem itemForward = new ToolItem(toolbar, SWT.PUSH);
+		itemForward.setText("Forward");
+		ToolItem itemStop = new ToolItem(toolbar, SWT.PUSH);
+		itemStop.setText("Stop");
+		ToolItem itemRefresh = new ToolItem(toolbar, SWT.PUSH);
+		itemRefresh.setText("Refresh");
+		ToolItem itemGo = new ToolItem(toolbar, SWT.PUSH);
+		itemGo.setText("Go");
 
-		myLabelInView = new Label(parent, SWT.BORDER);
-		myLabelInView.setText("This is a sample E4 view");
+		GridData data = new GridData();
+		data.horizontalSpan = 3;
+		toolbar.setLayoutData(data);
 
+		Label labelAddress = new Label(parent, SWT.NONE);
+		labelAddress.setText("Address");
+
+		final Text location = new Text(parent, SWT.BORDER);
+		data = new GridData();
+		data.horizontalAlignment = GridData.FILL;
+		data.horizontalSpan = 2;
+		data.grabExcessHorizontalSpace = true;
+		location.setLayoutData(data);
+
+		final Browser browser;
+		try {
+			browser = new Browser(parent, SWT.NONE);
+		} catch (SWTError e) {
+			System.out.println("Could not instantiate Browser: " + e.getMessage());
+			parent.dispose();
+			return;
+		}
+		data = new GridData();
+		data.horizontalAlignment = GridData.FILL;
+		data.verticalAlignment = GridData.FILL;
+		data.horizontalSpan = 3;
+		data.grabExcessHorizontalSpace = true;
+		data.grabExcessVerticalSpace = true;
+		browser.setLayoutData(data);
+
+		final Label status = new Label(parent, SWT.NONE);
+		data = new GridData(GridData.FILL_HORIZONTAL);
+		data.horizontalSpan = 2;
+		status.setLayoutData(data);
+
+		final ProgressBar progressBar = new ProgressBar(parent, SWT.NONE);
+		data = new GridData();
+		data.horizontalAlignment = GridData.END;
+		progressBar.setLayoutData(data);
+
+		/* event handling */
+		Listener listener = event -> {
+			ToolItem item = (ToolItem) event.widget;
+			String string = item.getText();
+			if (string.equals("Back"))
+				browser.back();
+			else if (string.equals("Forward"))
+				browser.forward();
+			else if (string.equals("Stop"))
+				browser.stop();
+			else if (string.equals("Refresh"))
+				browser.refresh();
+			else if (string.equals("Go"))
+				browser.setUrl(location.getText());
+		};
+		browser.addProgressListener(new ProgressListener() {
+			@Override
+			public void changed(ProgressEvent event) {
+					if (event.total == 0) return;
+					int ratio = event.current * 100 / event.total;
+					progressBar.setSelection(ratio);
+			}
+			@Override
+			public void completed(ProgressEvent event) {
+				progressBar.setSelection(0);
+			}
+		});
+		browser.addStatusTextListener(event -> status.setText(event.text));
+		browser.addLocationListener(LocationListener.changedAdapter(event -> {
+				if (event.top) location.setText(event.location);
+			}
+		));
+		itemBack.addListener(SWT.Selection, listener);
+		itemForward.addListener(SWT.Selection, listener);
+		itemStop.addListener(SWT.Selection, listener);
+		itemRefresh.addListener(SWT.Selection, listener);
+		itemGo.addListener(SWT.Selection, listener);
+		location.addListener(SWT.DefaultSelection, e -> browser.setUrl(location.getText()));
+
+		browser.setUrl("microsoft.com");
 	}
 
 	@Focus
